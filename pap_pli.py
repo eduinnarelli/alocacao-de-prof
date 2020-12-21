5d42b4199c19b97def5dd06f00a6396d042fd3b7
Bruno Richau <19270033+Brunomri@users.noreply.github.com>
2020-12-21 15:41:26 -0300

    Executa todas as instâncias .pap em um diretório e salva os resultados em um .csv

--- pap_pli.py
+++ pap_pli.py
@@ -1,6 +1,6 @@
 # Modelo de programação linear inteira para solucionar problema PAP
 
-import sys
+import sys, csv, os
 from os import path
 import numpy as np
 import gurobipy as gp
@@ -15,17 +15,17 @@ def instanceGen(f_name):
     f.close()
 
     # Obtem parametros das 5 primeiras linhas do arquivo
-    P = content[0][2:]
-    D = content[1][2:]
-    T = content[2][2:]
-    S = content[3][2:]
-    H = content[4][2:]
+    P = int(content[0][2:].replace('\n', ''))
+    D = int(content[1][2:].replace('\n', ''))
+    T = int(content[2][2:].replace('\n', ''))
+    S = int(content[3][2:].replace('\n', ''))
+    H = int(content[4][2:].replace('\n', ''))
 
     # Armazena valores de hd em um vetor de dimensao D x 1, obtem um valor por
     # linha e remove os caracteres de quebra de linha
     hd = np.zeros(shape = (int(D), 1), dtype = "int")
     for disc in range (len(hd)):
-        hd[disc] = content[disc + 6].replace('\n', '')
+        hd[disc] = int(content[disc + 6].replace('\n', ''))
     #print("hd length = {}".format(len(hd)))
     #print("hd:\n{}\n".format(hd))
 
@@ -68,13 +68,14 @@ def instanceGen(f_name):
                 t += 1
 
     #print("rpt = \n{}\n".format(rpt))
+    param = [P, D, T, S, H, hd, apd, rpt]
 
-    return P, D, T, S, H, hd, apd, rpt
+    return param
 
 def solve(P, D, T, S, H, hd, apd, rpt, f_name):
     try:
         # Criacao do modelo
-        model = gp.Model("PAP instancia {}\n".format(f_name))
+        model = gp.Model(f_name)
 
         # Variaveis de decisao
         # Variavel binaria x_{p}_{d} indica se o professor p é alocado a disciplina d
@@ -147,7 +148,8 @@ def solve(P, D, T, S, H, hd, apd, rpt, f_name):
             for p in range(P) for d in range(D) for t in range(T)}
 
         # Funcao objetivo
-        # Maximiza os professores com a maior avaliacao possivel alocados para cada disciplina
+        # Maximiza os professores com a maior avaliacao possivel alocados para cada disciplina e subtrai
+        # uma penalidade de valor 100 caso nao exista solucao
         exp1 = gp.quicksum(apd[p, d] * x_vars[p, d] 
                          for d in range(D) 
                          for p in range(P))
@@ -162,59 +164,89 @@ def solve(P, D, T, S, H, hd, apd, rpt, f_name):
         model.setParam(GRB.Param.TimeLimit, 1800.0)
         model.optimize()
 
-        if model.status == GRB.OPTIMAL:    
+        # Analisa resultados da otimizacao
+        if model.status == GRB.OPTIMAL:
             print("Nome do modelo: ", model.ModelName)
             print("Valor da funcao objetivo: ", model.ObjVal)
+            print("Tempo de execucao: ", model.Runtime)
+            print("Numero de iteracoes: ", model.IterCount)
             print("Numero de variaveis de decisao: ", model.NumVars)
             print("Numero de restricoes: ", model.NumConstrs)
             print("Numero de objetivos: ", model.NumObj)
-            print("Numero de iteracoes: ", model.IterCount)
-            print("Tempo de execucao: ", model.Runtime)
 
-        elif model.status == GRB.INF_OR_UNBD:    
-            print('Modelo inviavel ou ilimitado')    
+            with open("pap_pli_resultados.csv", 'a', newline = '') as file:
+                writer = csv.writer(file)
+                writer.writerow([model.ModelName, model.ObjVal, model.Runtime, model.IterCount, model.NumVars, model.NumConstrs, model.NumObj])
 
-        elif model.status == GRB.INFEASIBLE:    
-            print('Modelo inviavel')    
+        elif model.status == GRB.INF_OR_UNBD:
+            print('Modelo inviavel ou ilimitado\n')
 
-        elif model.status == GRB.UNBOUNDED:    
-            print('Modelo ilimitado')    
+        elif model.status == GRB.INFEASIBLE:
+            print('Modelo inviavel\n')
 
-        else:    
-            print('Otimizacao terminou com status %d' % model.status)    
+        elif model.status == GRB.UNBOUNDED:
+            print('Modelo ilimitado\n')
+
+        else:
+            print('Otimizacao terminou com status %d' % model.status)
 
     except gp.GurobiError as e:
         print('Codigo de erro ' + str(e.errno) + ': ' + str(e))
 
     except AttributeError:
-        print('Encontrado erro de atributo')
+        print('Encontrado erro de atributo\n')
 
 if __name__ == "__main__":
 
     # Verifica se arquivo de entrada foi fornecido corretamente
     if len(sys.argv) != 2:
-        sys.exit("Uso: python pap_pli.py instances\nome_do_arquivo.pap")
+        sys.exit("Uso: python pap_pli.py <diretorio com instacias .pap>")
     else:
-        f_path = sys.argv[1]
-        f_name = f_path[10:]
-        if path.exists(f_path):
-            print("Arquivo de entrada: {}\n".format(f_name))
+        # Gera instancias a partir dos arquivos de entrada .pap existentes
+        # no diretorio fornecido
+        dir = sys.argv[1]
 
-            # Gera instancias a partir do arquivo de entrada
-            P, D, T, S, H, hd, apd, rpt = instanceGen(f_path)
+        if path.exists(dir):
+            obj = os.scandir(dir)
+            f_dict = {}
 
-            # Imprime todos os parâmetros gerados
-            param = {"P" : P, "D" : D, "T" : T, "S" : S, "H" : H}
-            for input in param:
-                v = param.get(input).replace('\n','')
-                print("{} = {}\n".format(input, v))
+            # Escaneia o diretorio e armazena o nome do arquivo e seus parametros
+            # como um par chave e valor de um dicionario
+            print("Arquivos no diretorio {}:\n".format(dir))
+            for entry in obj :
+                if entry.is_dir() or entry.is_file():
+                    if entry.name[-4:] == ".pap":
+                        #print(entry.name)
+                        param = instanceGen(entry)
+                        #print(param)
+                        f_dict[entry.name] = param
 
-            print("hd = \n{}\n".format(hd))
-            print("apd = \n{}\n".format(apd))
-            print("rpt = \n{}\n".format(rpt))
+                    if len(f_dict) == 0:
+                        sys.exit("Erro: O diretorio {} nao possui arquivos .pap\n".format(dir))
+            #print(f_dict.items())
 
-            # Resolve a instancia via Gurobi
-            solve(int(P), int(D), int(T), int(S), int(H), hd, apd, rpt, f_name)
+            # Cria arquivo .csv para armazenar os resultados
+            with open("pap_pli_resultados.csv", 'a', newline = '') as file:
+                writer = csv.writer(file)
+                writer.writerow(["Instancia", "Valor Obj", "Tempo", "# Iteracoes", "# Variaveis", " # Restricoes", "# Obj"])
 
+            # Resolve todas as instancia via Gurobi
+            for inst in f_dict:
+                print("Instância {}\n".format(inst[:-4]))
+                param = f_dict.get(inst)
+
+                print("P = {}".format(param[0]))
+                print("D = {}".format(param[1]))
+                print("T = {}".format(param[2]))
+                print("S = {}".format(param[3]))
+                print("H = {}".format(param[4]))
+                print("hd shape = {}".format(param[5].shape))
+                #print("hd = \n{}\n".format(param[5]))
+                print("apd shape = {}".format(param[6].shape))
+                #print("apd = \n{}\n".format(param[6]))
+                print("rpt shape = {}\n".format(param[7].shape))
+                #print("rpt = \n{}\n".format(param[7]))
+
+                solve(param[0], param[1], param[2], param[3], param[4], param[5], param[6], param[7], inst[:-4])
         else:
-            sys.exit("Erro: O arquivo {} nao existe\n".format(f_path))
+            sys.exit("Erro: O diretorio {} nao existe\n".format(dir))
