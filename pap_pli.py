bb1e99746d246949604312fa6cc6af24f5a84fed
Eduardo Innarelli <eduardo.innarelli@neuralmind.ai>
2020-12-22 10:49:40 -0300

    Changed restriction 2.3 in `pap_ply.py`

--- pap_pli.py
+++ pap_pli.py
@@ -1,6 +1,8 @@
 # Modelo de programação linear inteira para solucionar problema PAP
 
-import sys, csv, os
+import sys
+import csv
+import os
 from os import path
 import numpy as np
 import gurobipy as gp
@@ -9,6 +11,8 @@ from gurobipy import quicksum
 
 # Realiza leitura do arquivo de entrada .pap e armazena os parâmetros
 # nas matrizes hd, apd e rpt
+
+
 def instanceGen(f_name):
     f = open(f_name, "r")
     content = f.readlines()
@@ -23,16 +27,16 @@ def instanceGen(f_name):
 
     # Armazena valores de hd em um vetor de dimensao D x 1, obtem um valor por
     # linha e remove os caracteres de quebra de linha
-    hd = np.zeros(shape = (int(D), 1), dtype = "int")
-    for disc in range (len(hd)):
+    hd = np.zeros(shape=(int(D), 1), dtype="int")
+    for disc in range(len(hd)):
         hd[disc] = int(content[disc + 6].replace('\n', ''))
     #print("hd length = {}".format(len(hd)))
-    #print("hd:\n{}\n".format(hd))
+    # print("hd:\n{}\n".format(hd))
 
     # Armazena valores de apd em uma matriz de dimensao P x D, obtem uma linha e
     # cada celula da matriz recebe o valor delimitado entre os espacos vazios de uma linha
-    apd = np.zeros(shape = (int(P), int(D)), dtype = "int")
-    for prof in range (len(apd)):
+    apd = np.zeros(shape=(int(P), int(D)), dtype="int")
+    for prof in range(len(apd)):
         line = content[7 + len(hd) + prof]
         #print("Linha {} = \n{}\n".format(prof, line))
         v = ""
@@ -53,8 +57,8 @@ def instanceGen(f_name):
 
     # Armazena valores de rpt em uma matriz de dimensao P x T, obtem uma linha e
     # cada celula da matriz recebe o valor delimitado entre os espacos vazios de uma linha
-    rpt = np.zeros(shape = (int(P), int(T)), dtype = "int")
-    for disp in range (len(rpt)):
+    rpt = np.zeros(shape=(int(P), int(T)), dtype="int")
+    for disp in range(len(rpt)):
         line = content[8 + len(hd) + len(apd) + disp]
         #print("Linha {} = \n{}\n".format(disp, line))
         t = 0
@@ -72,6 +76,7 @@ def instanceGen(f_name):
 
     return param
 
+
 def solve(P, D, T, S, H, hd, apd, rpt, f_name):
     try:
         # Criacao do modelo
@@ -79,83 +84,84 @@ def solve(P, D, T, S, H, hd, apd, rpt, f_name):
 
         # Variaveis de decisao
         # Variavel binaria x_{p}_{d} indica se o professor p é alocado a disciplina d
-        x_vars = {(p, d) : model.addVar(vtype = GRB.BINARY, name = "x_{0}_{1}".format(p, d))
+        x_vars = {(p, d): model.addVar(vtype=GRB.BINARY, name="x_{0}_{1}".format(p, d))
                   for p in range(P) for d in range(D)}
 
         # Variavel binaria y_{d}_{t} indica se a disciplina d é alocada no periodo t
-        y_vars = {(d, t) : model.addVar(vtype = GRB.BINARY, name = "y_{0}_{1}".format(d, t))
+        y_vars = {(d, t): model.addVar(vtype=GRB.BINARY, name="y_{0}_{1}".format(d, t))
                   for d in range(D) for t in range(T)}
 
         # Variavel binaria z_{p}_{t} indica se o professor p é alocado no periodo t
-        z_vars = {(p, t) : model.addVar(vtype = GRB.BINARY, name = "x_{0}_{1}".format(p, t))
+        z_vars = {(p, t): model.addVar(vtype=GRB.BINARY, name="x_{0}_{1}".format(p, t))
                   for p in range(P) for t in range(T)}
 
         # Restricoes
 
         # Garante que uma disciplina pode estar alocada a no maximo um professor
-        r0 = {(d) : 
-        model.addConstr(
-                lhs = gp.quicksum(x_vars[p, d] for p in range(P)),
-                sense = GRB.LESS_EQUAL,
-                rhs = 1, 
-                name = "r0_{0}".format(d))
+        r0 = {(d):
+              model.addConstr(
+            lhs=gp.quicksum(x_vars[p, d] for p in range(P)),
+            sense=GRB.LESS_EQUAL,
+            rhs=1,
+            name="r0_{0}".format(d))
             for d in range(D)}
 
         # Cada disciplina d exige hd periodos em uma semana
-        r1 = {(d) : 
-        model.addConstr(
-                lhs = gp.quicksum(y_vars[d, t] for t in range(T)),
-                sense = GRB.EQUAL,
-                rhs = hd[d], 
-                name = "r1_{0}".format(d))
+        r1 = {(d):
+              model.addConstr(
+            lhs=gp.quicksum(y_vars[d, t] for t in range(T)),
+            sense=GRB.EQUAL,
+            rhs=gp.quicksum(np.squeeze(hd)[d] * x_vars[p, d]
+                            for p in range(P)),
+            name="r1_{0}".format(d))
             for d in range(D)}
 
         # O instituto possui S salas, portanto no maximo S disciplinas podem ser ministradas no mesmo periodo
-        r2 = {(t) : 
-        model.addConstr(
-                lhs = gp.quicksum(y_vars[d, t] for d in range(D)),
-                sense = GRB.LESS_EQUAL,
-                rhs = S, 
-                name = "r2_{0}".format(t))
+        r2 = {(t):
+              model.addConstr(
+            lhs=gp.quicksum(y_vars[d, t] for d in range(D)),
+            sense=GRB.LESS_EQUAL,
+            rhs=S,
+            name="r2_{0}".format(t))
             for t in range(T)}
 
-        # Cada professor p possui restricoes de quais periodos nao pode lecionar, se r_{p}_{t} = 1 entao o 
+        # Cada professor p possui restricoes de quais periodos nao pode lecionar, se r_{p}_{t} = 1 entao o
         # professor p pode lecionar no periodo t. Caso contrario, r_{p}_{t} = 0
-        r3 = {(p, t) : 
-        model.addConstr(
-                lhs = z_vars[p, t],
-                sense = GRB.LESS_EQUAL,
-                rhs = rpt[p, t], 
-                name = "r3_{0}_{1}".format(p, t))
+        r3 = {(p, t):
+              model.addConstr(
+            lhs=z_vars[p, t],
+            sense=GRB.LESS_EQUAL,
+            rhs=rpt[p, t],
+            name="r3_{0}_{1}".format(p, t))
             for p in range(P) for t in range(T)}
 
         # Um professor pode estar alocado a mais de uma disciplina, no entanto a carga de disciplinas do professor deve somar no maximo H períodos
-        r4 = {(p) : 
-        model.addConstr(
-                lhs = gp.quicksum(z_vars[p, t] for t in range(T)),
-                sense = GRB.LESS_EQUAL,
-                rhs = H, 
-                name = "r4_{0}".format(p))
+        r4 = {(p):
+              model.addConstr(
+            lhs=gp.quicksum(z_vars[p, t] for t in range(T)),
+            sense=GRB.LESS_EQUAL,
+            rhs=H,
+            name="r4_{0}".format(p))
             for p in range(P)}
 
         # Se um professor e alocado a uma disciplina e esta e alocada num período, o professor e alocado no mesmo período
-        r5 = {(p, d, t) : 
-        model.addConstr(
-                lhs = x_vars[p, d] + y_vars[d, t] - 1,
-                sense = GRB.LESS_EQUAL,
-                rhs = z_vars[p, t], 
-                name = "r5_{0}_{1}_{2}".format(p, d, t))
+        r5 = {(p, d, t):
+              model.addConstr(
+            lhs=x_vars[p, d] + y_vars[d, t] - 1,
+            sense=GRB.LESS_EQUAL,
+            rhs=z_vars[p, t],
+            name="r5_{0}_{1}_{2}".format(p, d, t))
             for p in range(P) for d in range(D) for t in range(T)}
 
         # Funcao objetivo
         # Maximiza os professores com a maior avaliacao possivel alocados para cada disciplina e subtrai
         # uma penalidade de valor 100 caso nao exista solucao
-        exp1 = gp.quicksum(apd[p, d] * x_vars[p, d] 
-                         for d in range(D) 
-                         for p in range(P))
+        exp1 = gp.quicksum(apd[p, d] * x_vars[p, d]
+                           for d in range(D)
+                           for p in range(P))
 
-        exp2 = 100 * gp.quicksum(1 - quicksum(x_vars[p, d] for p in range(P)) 
-                         for d in range(D))
+        exp2 = 100 * gp.quicksum(1 - quicksum(x_vars[p, d] for p in range(P))
+                                 for d in range(D))
 
         exp = exp1 - exp2
         model.setObjective(exp, GRB.MAXIMIZE)
@@ -174,9 +180,10 @@ def solve(P, D, T, S, H, hd, apd, rpt, f_name):
             print("Numero de restricoes: ", model.NumConstrs)
             print("Numero de objetivos: ", model.NumObj)
 
-            with open("pap_pli_resultados.csv", 'a', newline = '') as file:
+            with open("pap_pli_resultados.csv", 'a', newline='') as file:
                 writer = csv.writer(file)
-                writer.writerow([model.ModelName, model.ObjVal, model.Runtime, model.IterCount, model.NumVars, model.NumConstrs, model.NumObj])
+                writer.writerow([model.ModelName, model.ObjVal, model.Runtime,
+                                 model.IterCount, model.NumVars, model.NumConstrs, model.NumObj])
 
         elif model.status == GRB.INF_OR_UNBD:
             print('Modelo inviavel ou ilimitado\n')
@@ -196,6 +203,7 @@ def solve(P, D, T, S, H, hd, apd, rpt, f_name):
     except AttributeError:
         print('Encontrado erro de atributo\n')
 
+
 if __name__ == "__main__":
 
     # Verifica se arquivo de entrada foi fornecido corretamente
@@ -213,22 +221,24 @@ if __name__ == "__main__":
             # Escaneia o diretorio e armazena o nome do arquivo e seus parametros
             # como um par chave e valor de um dicionario
             print("Arquivos no diretorio {}:\n".format(dir))
-            for entry in obj :
+            for entry in obj:
                 if entry.is_dir() or entry.is_file():
                     if entry.name[-4:] == ".pap":
-                        #print(entry.name)
+                        # print(entry.name)
                         param = instanceGen(entry)
-                        #print(param)
+                        # print(param)
                         f_dict[entry.name] = param
 
                     if len(f_dict) == 0:
-                        sys.exit("Erro: O diretorio {} nao possui arquivos .pap\n".format(dir))
-            #print(f_dict.items())
+                        sys.exit(
+                            "Erro: O diretorio {} nao possui arquivos .pap\n".format(dir))
+            # print(f_dict.items())
 
             # Cria arquivo .csv para armazenar os resultados
-            with open("pap_pli_resultados.csv", 'a', newline = '') as file:
+            with open("pap_pli_resultados.csv", 'a', newline='') as file:
                 writer = csv.writer(file)
-                writer.writerow(["Instancia", "Valor Obj", "Tempo", "# Iteracoes", "# Variaveis", " # Restricoes", "# Obj"])
+                writer.writerow(["Instancia", "Valor Obj", "Tempo",
+                                 "# Iteracoes", "# Variaveis", " # Restricoes", "# Obj"])
 
             # Resolve todas as instancia via Gurobi
             for inst in f_dict:
@@ -247,6 +257,7 @@ if __name__ == "__main__":
                 print("rpt shape = {}\n".format(param[7].shape))
                 #print("rpt = \n{}\n".format(param[7]))
 
-                solve(param[0], param[1], param[2], param[3], param[4], param[5], param[6], param[7], inst[:-4])
+                solve(param[0], param[1], param[2], param[3],
+                      param[4], param[5], param[6], param[7], inst[:-4])
         else:
             sys.exit("Erro: O diretorio {} nao existe\n".format(dir))
